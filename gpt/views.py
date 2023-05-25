1e8bc4d067134e22c9fb7dcbbbd91a6a4576949e
Tomo Norman <tomo.l.norman@gmail.com>
2023-05-25 11:23:56 +0900

    using openai

--- gpt/views.py
+++ gpt/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_KEY')
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 150,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
 
 # Create your views here.
 def chatbot (request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = "Hi this is my response"
-        return JsonResponse({'message': message, 'response': response})
+        response = ask_openai(message)
+        return JsonResponse({'response': response})
     return render(request, "chatbot.html")
