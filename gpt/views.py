53bb64dd9264b4b09766287e81ffe6b325e88ae6
Tomo Norman <tomo.l.norman@gmail.com>
2023-05-29 09:49:51 +0900

    use gpt3.5 model

--- gpt/views.py
+++ gpt/views.py
@@ -13,16 +13,15 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_KEY')
 
 def ask_openai(message):
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = message,
-        max_tokens = 150,
-        n = 1,
-        stop = None,
-        temperature = 0.7,
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ]
     )
 
-    answer = response.choices[0].text.strip()
+    answer = response.choices[0].message.content.strip()
     return answer
 
 # Create your views here.
